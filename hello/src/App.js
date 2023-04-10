069249ba287e3c047319cdada456920515283501
Jack Chau <chaukaho149@gmail.com>
2023-04-10 18:36:01 +0800

    useState

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Employee from './components/Employee'
+import { useState } from 'react' ;
 
 function App() {
-  console.log( "we are about to list the employees" ) ;
+  const [ role, setRole ] = useState( 'dev' ) ;
+
   const showEmployees = true ;
   return (
     <div className="App">
@@ -10,9 +12,17 @@ function App() {
       { showEmployees ? ( 
       // if ture 
         <>
+        <input 
+          type = "text" 
+          onChange = { ( e ) => {
+          console.log( e.target.value ) ;
+          setRole( e.target.value ) ;
+          } } 
+        />
+
           <Employee name="Jack" role="Intern"/>
           <Employee name="Calabe" role="Professor"/>
-          <Employee name="Abby"/>
+          <Employee name="Abby" role={ role }/>
         </>
       // if false
       ) : ( 
--- hello/src/components/Employee.js
+++ hello/src/components/Employee.js
@@ -1,15 +1,16 @@
 function Employee( props ) {
+    
     return (
 
         <>
             <h3>Employee { props.name } </h3>
 
             { props.role ? (
-                <p class = "role" > { props.role } </p>
+                <p className = "role" > { props.role } </p>
             ) : (
-                <p class = "no role" > No Role </p> 
+                <p className = "no role" > No Role </p> 
             ) }
-            
+
         </>
 
     )
